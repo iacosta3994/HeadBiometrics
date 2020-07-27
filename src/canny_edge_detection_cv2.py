f0c8ef7ca0957af6b4a933e3886e2b42087ab9ad
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-07-27 16:42:26 -0700

    Commented out testing block

--- src/canny_edge_detection_cv2.py
+++ src/canny_edge_detection_cv2.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-def make_canny(filename,filename_canny):
+def make_canny(filename, filename_canny):
     img = cv2.imread(filename,0) #pulling img
     edges = cv2.Canny(img,100,200)              #making img canny
     cv2.imwrite(filename_canny, edges) #overwrite startingg frame with canny version
@@ -13,3 +13,9 @@ def make_canny(filename,filename_canny):
     plt.subplot(122),plt.imshow(edges,cmap = 'gray')
     plt.title('Canny Image'), plt.xticks([]), plt.yticks([])
     plt.show()
+
+"""cap_origin_video, saved_frames_location,  frames_to_skip, current_frame_idx  = ('Video_Tests\A_Tilt_Side_to_Side.mp4'), ('./data/A_frame') , 30 , 0
+filename = saved_frames_location + str(current_frame_idx) + '.jpeg'
+filename_canny = saved_frames_location + '_canny'+ str(current_frame_idx) +  '.jpeg'
+
+make_canny(filename, filename_canny)"""
