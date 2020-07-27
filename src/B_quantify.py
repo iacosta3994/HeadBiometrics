3563c0dc35c5c3cd5880fe7bbb02bef8ed8b30e6
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-07-26 22:21:19 -0700

    added explinatins to AB, keyframe made into function, magstripe started

---
+++ src/B_quantify.py
@@ -0,0 +1,5 @@
+'''
+This will be in charge of preparing B_photos (the side of the head). Then it will need to determine the front_to_back, length, circumference and ear_to_ear in pixels.
+User input will be needed to find front_to_back due to dynamic hairlines in individuals heads.
+then use info from mag_stripe_search to get the conversion from pixel to mm^2 giving front_to_back, length, circumference and ear_to_ear in mm
+'''
---
+++ src/mag_stripe_search.py
@@ -0,0 +1,6 @@
+'''
+Inputs will be a database of images, that have already been modified with canny.
+Function will look for a set of pixels with same ratio demensions as mag_stripe.
+
+With the image, Height and Width (pixel_H, pixel_W : 0 , 0)will be saved along with specific frameIdx it belongs to
+This will help determine wich frame has the Highest Height & Width, new py that esablishes collection of values to find Highest values will be output
