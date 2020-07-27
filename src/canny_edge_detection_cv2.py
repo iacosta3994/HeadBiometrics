9f302d1ea6d2ccb2500ccd5c40e168a59d985a41
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-07-27 09:03:29 -0700

    set py into a function make_canny takes starting location input

--- src/canny_edge_detection_cv2.py
+++ src/canny_edge_detection_cv2.py
@@ -2,12 +2,13 @@ import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
 
+def make_canny(starting_frame_location):
 #starting off with the first fram in the database [frame0]
-img = cv.imread('./data/frame/frame0.jpg',0)
-edges = cv.Canny(img,100,200)
+    img = cv.imread(starting_frame_location,0)
+    edges = cv.Canny(img,100,200)
 
-plt.subplot(121),plt.imshow(img,cmap = 'gray')
-plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(edges,cmap = 'gray')
-plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
-plt.show()
+    plt.subplot(121),plt.imshow(img,cmap = 'gray')
+    plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+    plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+    plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+    plt.show()
