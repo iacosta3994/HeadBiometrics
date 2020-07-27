01fa289cf2c729fbb988302eeb913fcf197c8b18
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-07-27 13:41:24 -0700

    set global variable, updates before else statement returning global var

--- src/key_frame_extraction.py
+++ src/key_frame_extraction.py
@@ -5,8 +5,7 @@ import os
 
 #Change in future to allow manipulation from __main__
 #cap_origin_video, saved_frames_location,  frames_to_skip  = ('Video_Tests\A_Tilt_Side_to_Side.mp4'), ('./data/frame') , 10
- #setting int to be used in AB quantify
-max_frame_idx = 'None!'
+global final_frame_idx
 #function splits incoming mp4 and sends frames to saved_frames_locations
 def split_frames(cap_origin_video, saved_frames_location, frames_to_skip):
 
@@ -22,7 +21,6 @@ def split_frames(cap_origin_video, saved_frames_location, frames_to_skip):
 
     current_frame_idx = 0 #starting with the first scene
 
-
     while cap.isOpened():
         #Establishing filename with the sequential number of frame
         filename = saved_frames_location + str(current_frame_idx) + '.jpeg'
@@ -33,11 +31,9 @@ def split_frames(cap_origin_video, saved_frames_location, frames_to_skip):
         #True if an image was retrieved
         if retrieved:
             cv2.imwrite(filename, frame)
+            final_frame_idx = current_frame_idx #sets final frame idx to the value of current frame idx before returning final_frame_idx
             current_frame_idx += frames_to_skip # determines how many frames to skip by
-
             cap.set(1, current_frame_idx) #sets (propId, value) allows to skip to later frame in cap
         else: #No image input
-            global max_frame_idx
-            max_frame_idx = current_frame_idx
             cap.release() #deallocates memory and clears capture pointer
-            break
+            return(final_frame_idx) #return value will be used to determine stopping frame in A&B_quantify
