7bf6abb8fe7d22d12f5b5a711fe4ed27e588aa87
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-07-27 15:52:03 -0700

    remove irrelevant notes, moved note on L15 made notes easier to read

--- src/key_frame_extraction.py
+++ src/key_frame_extraction.py
@@ -2,12 +2,9 @@ import cv2
 import numpy as np
 import os
 
+global final_frame_idx #setting as global to allow update in function and able to return for parent function A_quantify
 
-#Change in future to allow manipulation from __main__
-#cap_origin_video, saved_frames_location,  frames_to_skip  = ('Video_Tests\A_Tilt_Side_to_Side.mp4'), ('./data/frame') , 10
-global final_frame_idx
-#function splits incoming mp4 and sends frames to saved_frames_locations
-def split_frames(cap_origin_video, saved_frames_location, frames_to_skip):
+def split_frames(cap_origin_video, saved_frames_location, frames_to_skip): #function splits incoming mp4 and sends frames to saved_frames_locations
 
     try:
         if not os.path.exists('data'):
@@ -15,12 +12,8 @@ def split_frames(cap_origin_video, saved_frames_location, frames_to_skip):
     except OSError:
         print ('Error: Creating directory of data')
 
-
-    # Assigning video from file to cap:
-    cap = cv2.VideoCapture(cap_origin_video)
-
+    cap = cv2.VideoCapture(cap_origin_video) # Assigning video from file to cap
     current_frame_idx = 0 #starting with the first scene
-
     while cap.isOpened():
         #Establishing filename with the sequential number of frame
         filename = saved_frames_location + str(current_frame_idx) + '.jpeg'
@@ -31,9 +24,9 @@ def split_frames(cap_origin_video, saved_frames_location, frames_to_skip):
         #True if an image was retrieved
         if retrieved:
             cv2.imwrite(filename, frame)
-            final_frame_idx = current_frame_idx #sets final frame idx to the value of current frame idx before returning final_frame_idx
-            current_frame_idx += frames_to_skip # determines how many frames to skip by
-            cap.set(1, current_frame_idx) #sets (propId, value) allows to skip to later frame in cap
+            final_frame_idx = current_frame_idx #final frame idx to the value of current frame idx before returning final_frame_idx
+            current_frame_idx += frames_to_skip #determines how many frames to skip by
+            cap.set(1, current_frame_idx)       #set(propId, value) allows to skip to later frame in cap
         else: #No image input
             cap.release() #deallocates memory and clears capture pointer
             return(final_frame_idx) #return value will be used to determine stopping frame in A&B_quantify
