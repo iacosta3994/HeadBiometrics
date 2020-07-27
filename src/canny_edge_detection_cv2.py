0cd3a7cc24a6c1998393938c3ad5ac0d24ff9788
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-07-27 15:52:40 -0700

    removed alias, added new term fore makecanny

--- src/canny_edge_detection_cv2.py
+++ src/canny_edge_detection_cv2.py
@@ -1,14 +1,15 @@
 import numpy as np
-import cv2 as cv
+import cv2
 from matplotlib import pyplot as plt
 
-def make_canny(starting_frame_location):
-#starting off with the first fram in the database [frame0]
-    img = cv.imread(starting_frame_location,0)
-    edges = cv.Canny(img,100,200)
+def make_canny(filename,filename_canny):
+    img = cv2.imread(filename,0) #pulling img
+    edges = cv2.Canny(img,100,200)              #making img canny
+    cv2.imwrite(filename_canny, edges) #overwrite startingg frame with canny version
+
 
     plt.subplot(121),plt.imshow(img,cmap = 'gray')
     plt.title('Original Image'), plt.xticks([]), plt.yticks([])
     plt.subplot(122),plt.imshow(edges,cmap = 'gray')
-    plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+    plt.title('Canny Image'), plt.xticks([]), plt.yticks([])
     plt.show()
