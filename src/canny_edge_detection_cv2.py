6f432343ca60b0c608ee8edc85977ca6def7c1e0
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-07-28 12:09:27 -0700

    removing visual test

--- src/canny_edge_detection_cv2.py
+++ src/canny_edge_detection_cv2.py
@@ -7,8 +7,8 @@ def make_canny(filename, filename_canny):
     edges = cv2.Canny(img,100,200) #making img canny
     cv2.imwrite(filename_canny, edges) #writes frame with canny version
 
-    plt.subplot(121),plt.imshow(img,cmap = 'gray')
-    plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-    plt.subplot(122),plt.imshow(edges,cmap = 'gray')
-    plt.title('Canny Image'), plt.xticks([]), plt.yticks([])
-    plt.show()
+    #plt.subplot(121),plt.imshow(img,cmap = 'gray')
+    #plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+    #plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+    #plt.title('Canny Image'), plt.xticks([]), plt.yticks([])
+    #plt.show()
