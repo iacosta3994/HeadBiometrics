140b4b6569f713a0728c956f5f5aa2f5b2d46fe8
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-07-28 12:38:58 -0700

    updated format to match A_quantify

--- src/B_quantify.py
+++ src/B_quantify.py
@@ -8,26 +8,20 @@ import sys
 import numpy as np
 import matplotlib
 import cv2
-
-import ast
-import inspect
-def contains_explicit_return(f):
-    return any(isinstance(node, ast.Return) for node in ast.walk(ast.parse(inspect.getsource(f))))
-
 from src.key_frame_extraction import *
 from src.canny_edge_detection_cv2 import *
 
-cap_origin_video, saved_frames_location,  frames_to_skip  = ('Video_Tests\B_Turn_Left_Right.mp4'), ('./data/B_frame') , 10
+
+cap_origin_video, saved_frames_location,  frames_to_skip  = ('Video_Tests\B_Turn_Left_Right.mp4'), ('./data/B_frame') , 30
 
 final_frame_idx = split_frames(cap_origin_video, saved_frames_location, frames_to_skip)
-
 current_frame_idx = 0
 
 while os.path:
-    filename = saved_frames_location + str(current_frame_idx) + '.jpeg'
+    filename, filename_canny = (saved_frames_location + str(current_frame_idx) + '.jpeg') , (saved_frames_location + '_canny'+ str(current_frame_idx) +  '.jpeg')
     with open('filename', 'w') as f:
         if current_frame_idx <= final_frame_idx:
-            make_canny(filename)
+            make_canny(filename, filename_canny)
             print("Current frame: {} Final frame : {}".format(current_frame_idx, final_frame_idx))
             current_frame_idx += frames_to_skip
         else:
