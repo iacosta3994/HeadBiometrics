152e3d465ee630d88e3655b9eceb0c9b0b125583
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-07-28 19:06:29 -0700

    reverting back to filename, removed gray convert.

--- src/canny_edge_detection_cv2.py
+++ src/canny_edge_detection_cv2.py
@@ -2,12 +2,11 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-def make_canny(file_path, file_path_canny):         # Function will load frame from file_path and will save canny edge image in same directory with new name
-    img = cv2.imread(file_path,0)                       # Assigning variable img from frame in file_path
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)        # Removing BGR data leaving gray image
-    blurred_img = cv2.GaussianBlur(gray, (3, 3), 0)     # Blurring to reduce artifacts
-    auto_canny(img)                             # Create edges around  image with auto min and max values
-    cv2.imwrite(file_path_canny, img_canny)             # Writes frame with canny name to file path
+def make_canny(filename, filename_canny):         # Function will load frame from file_path and will save canny edge image in same directory with new name
+    image = cv2.imread(filename,0)                       # Assigning variable img from frame in file_path
+    blurred_img = cv2.GaussianBlur(image, (3, 3), 0)     # Blurring to reduce artifacts
+    img_canny = auto_canny(blurred_img)                  # Create edges around  image with auto min and max values
+    cv2.imwrite(filename_canny, img_canny)             # Writes frame with canny name to file path
 
 def auto_canny(img, sigma=0.33):                    # Runs an automated version of canny with optimal lower and upper thresholds
     v = np.median(img)                                  # Gets median value of channel intensity | unique for each image
@@ -16,7 +15,7 @@ def auto_canny(img, sigma=0.33):                    # Runs an automated version 
     img_canny = cv2.Canny(img , lower , upper)             # Img_canny now has the canny edge image
     return img_canny
 
-    
+
     #plt.subplot(121),plt.imshow(img,cmap = 'gray')
     #plt.title('Original Image'), plt.xticks([]), plt.yticks([])
     #plt.subplot(122),plt.imshow(edges,cmap = 'gray')
