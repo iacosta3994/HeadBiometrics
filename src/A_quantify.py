5743a04041383ac318384f6474d2baf2173b3cf3
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-07-31 01:41:14 -0700

    mag_stripe_constant(mm) divided by avg mag_stripe| prints mm per pixel

--- src/A_quantify.py
+++ src/A_quantify.py
@@ -12,12 +12,15 @@ from src.key_frame_extraction import *
 from src.canny_edge_detection_cv2 import *
 from src.mag_stripe_search import *
 
-cap_origin_video, saved_frames_location,  frames_to_skip  = ('Video_Tests\A_Tilt_Side_to_Side.mp4'), ('./data/A_frame') , 10
+cap_origin_video, saved_frames_location,  frames_to_skip  = ('Video_Tests\A_Tilt_Side_to_Side.mp4'), ('./data/A_frame') , 1
 
 final_frame_idx = split_frames(cap_origin_video, saved_frames_location, frames_to_skip)
 current_frame_idx = 0
 
-mag_stripe_constant_w_h =[]
+list_mag_stripe_w_h =[]
+mag_stripe_constant_w_h = [85.60, 8.37]
+
+
 while os.path:
 
     filename, filename_canny = saved_frames_location + str(current_frame_idx) + '.jpeg' , saved_frames_location + '_canny'+ str(current_frame_idx) +  '.jpeg'
@@ -27,9 +30,10 @@ while os.path:
             canny_image = make_canny(filename, filename_canny)
             mag_stripe_w_h = get_magstripe_demensions(canny_image, filename_canny)
             if mag_stripe_w_h:
-                print(mag_stripe_w_h)
+                list_mag_stripe_w_h.append(mag_stripe_w_h)
             #print("Current frame: {} Final frame : {}".format(current_frame_idx, final_frame_idx))
             current_frame_idx += frames_to_skip
         else:
-
+            mean_mag_stripe_w_h = (np.mean(list_mag_stripe_w_h, axis=0))
+            print(np.divide(mag_stripe_constant_w_h, mean_mag_stripe_w_h))
             break
