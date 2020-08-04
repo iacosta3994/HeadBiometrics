cc86df5cef731160bf432f5ebd6102a201c1033f
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-08-04 16:36:13 -0700

    added threshhold to exclusive to darker pixels.

--- src/canny_edge_detection_cv2.py
+++ src/canny_edge_detection_cv2.py
@@ -2,17 +2,20 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-def make_canny(filename, filename_canny):         # Function will load frame from file_path and will save canny edge image in same directory with new name
-    image = cv2.imread(filename,0)                       # Assigning variable img from frame in file_path
-    blurred_img = cv2.GaussianBlur(image, (3,3), 0)     # Blurring to reduce artifacts
-    img_canny = auto_canny(blurred_img)                  # Create edges around  image with auto min and max values
-    cv2.imwrite(filename_canny, img_canny)             # Writes frame with canny name to file path
-    return img_canny
-def auto_canny(img, sigma=0.33):                    # Runs an automated version of canny with optimal lower and upper thresholds || sigma .33 Default
-    v = np.median(img)                                  # Gets median value of channel intensity | unique for each image
-    lower = int(max(0, (1.0 - sigma) * v))              # Sets lower int valeue for canny function
-    upper = int(min(255, (1.0 + sigma)* v))             # Sets higher int value for canny function
-    img_canny = cv2.Canny(img , lower , upper)             # Img_canny now has the canny edge image
+def make_canny(filename, filename_canny):                                # Function will load frame from file_path and will save canny edge image in same directory with new name
+    image = cv2.imread(filename,0)                                          # Assigning variable img from frame in file_path
+    blurred_img = cv2.GaussianBlur(image, (5,5), 0)                        # Blurring to reduce artifacts
+    ret,thresh1 = cv2.threshold(blurred_img,30,255,cv2.THRESH_BINARY)              # Sets a threshold in search for the pixels near the color of black i.e magstripe
+    img_canny = auto_canny(thresh1)                                       # Create edges around  image with auto min and max values
+    kernel = np.ones((3,3),np.uint8)                                        # Sets a kernal 3*3 used for canny img_canny_dilation
+    img_canny_dilation = cv2.dilate(img_canny, kernel, iterations = 1)
+    cv2.imwrite(filename_canny, img_canny_dilation)                           # Writes frame with canny name to file path
+    return img_canny_dilation
+def auto_canny(img, sigma=0.33):                                     # Runs an automated version of canny with optimal lower and upper thresholds || sigma .33 Default
+    v = np.median(img)                                                   # Gets median value of channel intensity | unique for each image
+    lower = int(max(0, (1.0 - sigma) * v))                              # Sets lower int valeue for canny function
+    upper = int(min(255, (1.0 + sigma)* v))                          # Sets higher int value for canny function
+    img_canny = cv2.Canny(img , lower , upper)                          # Img_canny now has the canny edge image
     return img_canny
 
 
