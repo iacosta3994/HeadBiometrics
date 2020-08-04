ccb949a948a3c2f10e050902d68cd01f734fd0e4
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-08-03 19:10:00 -0700

    updating b with a, swapped h w for portrait photo being used in landscap

--- src/B_quantify.py
+++ src/B_quantify.py
@@ -10,12 +10,17 @@ import matplotlib
 import cv2
 from src.key_frame_extraction import *
 from src.canny_edge_detection_cv2 import *
+from src.mag_stripe_search import *
 
 
-cap_origin_video, saved_frames_location,  frames_to_skip  = ('Video_Tests\B_Turn_Left_Right.mp4'), ('./data/B_frame') , 10
+cap_origin_video, saved_frames_location,  frames_to_skip  = ('Video_Tests\B_test_self.mp4'), ('./data/B_frame') , 1
 
 final_frame_idx = split_frames(cap_origin_video, saved_frames_location, frames_to_skip)
 current_frame_idx = 0
+
+list_mag_stripe_h_w =[]
+mag_stripe_constant_h_w = [8.37, 85.60]
+
 
 while os.path:
 
@@ -24,8 +29,15 @@ while os.path:
     with open('filename', 'w') as f:
         if current_frame_idx <= final_frame_idx:
             canny_image = make_canny(filename, filename_canny)
-            get_magstripe_demensions(canny_image, filename_canny)
+            mag_stripe_h_w = get_magstripe_demensions(canny_image, filename_canny)
+            if mag_stripe_h_w:
+                list_mag_stripe_h_w.append(mag_stripe_h_w)
             #print("Current frame: {} Final frame : {}".format(current_frame_idx, final_frame_idx))
             current_frame_idx += frames_to_skip
         else:
+            if len(list_mag_stripe_h_w) == 0:
+                print("found no magstripe")
+                break
+            mean_mag_stripe_h_w = (np.mean(list_mag_stripe_h_w, axis=0))
+            print(np.divide(mag_stripe_constant_h_w, mean_mag_stripe_h_w))
             break
