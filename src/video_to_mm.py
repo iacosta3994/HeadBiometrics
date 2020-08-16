07cb62104b876c7ebdf324138e95ed1e81f27f3b
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-08-16 16:03:32 -0700

    added notes to explain each line, removed print current frame out of tot

--- src/video_to_mm.py
+++ src/video_to_mm.py
@@ -12,37 +12,37 @@ from src.key_frame_extraction import *
 from src.canny_edge_detection_cv2 import *
 from src.mag_stripe_search import *
 
-def video_to_pixel_mm (origin_video, saved_frame_name, use_every_x_frame):
+def video_to_pixel_mm (origin_video, saved_frame_name, use_every_x_frame):                                              # Funtion that inputs video mp4, name of scene with location, variable to determine how many frames to splice
 
-    cap_origin_video, saved_frames_location,  frames_to_skip  = (origin_video, saved_frame_name , use_every_x_frame)
+    cap_origin_video, saved_frames_location,  frames_to_skip  = (origin_video, saved_frame_name , use_every_x_frame)    # Assinging the input variables to local variables for Function
 
-    final_frame_idx = split_frames(cap_origin_video, saved_frames_location, frames_to_skip)
-    current_frame_idx = 0
+    final_frame_idx = split_frames(cap_origin_video, saved_frames_location, frames_to_skip)                             # Splits the frame then returns the frame that is being worked on
+    current_frame_idx = 0                                                                                               # Starting with the first frame
 
-    list_mag_stripe_w_h =[]
-    mag_stripe_constant_w_h = [85.60, 8.37]
+    list_mag_stripe_w_h =[]                                                                                             # Later this list will have all the H and W of the mag stripe
+    mag_stripe_constant_w_h = [85.60, 8.37]                                                                             # Measured Magstripe is 85.60 mm wide and 8.37 mm tall
 
 
     while os.path:
 
-        filename, filename_canny = saved_frames_location + str(current_frame_idx) + '.jpeg' , saved_frames_location + '_canny'+ str(current_frame_idx) +  '.jpeg'
+        filename = saved_frames_location + str(current_frame_idx) + '.jpeg'                                             # The name of the frame being proccesed
+        filename_canny = saved_frames_location + '_canny'+ str(current_frame_idx) +  '.jpeg'                            # Similar to prior but indicates the version with canny that is used for printing out the test
 
-        with open('filename', 'w') as f:
-            if current_frame_idx <= final_frame_idx:
-                canny_image = make_canny_magstripe(filename, filename_canny)
-                mag_stripe_w_h = get_magstripe_demensions(canny_image, filename_canny)
-                if mag_stripe_w_h:
-                    list_mag_stripe_w_h.append(mag_stripe_w_h)
-                #print("Current frame: {} Final frame : {}".format(current_frame_idx, final_frame_idx))
-                current_frame_idx += frames_to_skip
-            else:
-                if len(list_mag_stripe_w_h) == 0:
+        with open('filename', 'w') as f:                                                                                # While there are frames to process continue
+            if current_frame_idx <= final_frame_idx:                                                                    # If the current frame is still part of the video || continue
+                canny_image = make_canny_magstripe(filename, filename_canny)                                            # Variable now has the canny image to process
+                mag_stripe_w_h = get_magstripe_demensions(canny_image, filename_canny)                                  # canny_image var is then used with get_magstripe_demensions
+                if mag_stripe_w_h:                                                                                      # It checks if any width and height was in the var
+                    list_mag_stripe_w_h.append(mag_stripe_w_h)                                                          # Appends data into list mag stripe to be consolidated after it finishes with loop
+                current_frame_idx += frames_to_skip                                                                     # Ticks the counter for the next idx
+            else:                                                                                                       # Once all the frames have been processed continue below
+                if len(list_mag_stripe_w_h) == 0:                                                                       # If list_mag_stripe_w_h didnt return anything stop the loop
                     print("found no magstripe")
                     break
-                if ((len(list_mag_stripe_w_h)) > 10):
-                    list_mag_stripe_w_h = sorted(list_mag_stripe_w_h)
-                    list_mag_stripe_w_h = list_mag_stripe_w_h[1:]
-                mean_mag_stripe_w_h = (np.mean(list_mag_stripe_w_h, axis=0))
-                pixel_mm_w_h = (np.divide(mag_stripe_constant_w_h, mean_mag_stripe_w_h))
-                pixel_mm_mean = ((pixel_mm_w_h[0] + pixel_mm_w_h[1])/2)
+                if ((len(list_mag_stripe_w_h)) > 10):                                                                   # Checks if lsit of data is longer than 10 recorded numbers
+                    list_mag_stripe_w_h = sorted(list_mag_stripe_w_h)                                                   # Sorts the list
+                    list_mag_stripe_w_h = list_mag_stripe_w_h[1:]                                                       # Removes the smallest var in list
+                mean_mag_stripe_w_h = (np.mean(list_mag_stripe_w_h, axis=0))                                            # Creates an avg of the W and H variables
+                pixel_mm_w_h = (np.divide(mag_stripe_constant_w_h, mean_mag_stripe_w_h))                                # This divides measured H and W witht the avg this will have a H and W square
+                pixel_mm_mean = ((pixel_mm_w_h[0] + pixel_mm_w_h[1])/2)                                                 # Removes the variable from a square to an avg unit of h and w
                 return pixel_mm_mean
