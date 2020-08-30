44c1b3974aeb8a0f15cd7ba0557e9c089019c1fe
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-08-30 14:53:05 -0700

    start of image segmentation using area largest and smallest

--- src/Narrow_Wide_img_select.py
+++ src/Narrow_Wide_img_select.py
@@ -1,1 +1,6 @@
 ''' this moduloe wil have 2 functions to find the narrowest object and the widest to determine which image to use to canny and pathfind. '''
+
+from src.canny_edge_detection_cv2 import *
+import cv2
+
+def narrowest_img (filename, ):     # Inputs frame-filename to scan for the narrowest head img
