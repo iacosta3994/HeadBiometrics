92db309df3e259a0595011396f2159c8eb9f1a1e
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-08-31 11:44:12 -0700

    relaxed epsilon, remove redundant var, aspctrtio narrowed, enabled testF

--- src/mag_stripe_search.py
+++ src/mag_stripe_search.py
@@ -21,14 +21,14 @@ def test_draw_magstripe_contour(contour, filename_canny, color = (0,255,0)):    
 
 def detect_mag_stripe(contours, filename_canny):                                                    # Narrows down the contours in search for the one that best fits
     for contour in contours:
-        epsilon = (0.03 * cv2.arcLength(contour, True))                                             # Calculate epsilon  using
+        epsilon = (0.02 * cv2.arcLength(contour, True))                                              # Epsilon helps account for possible inperfections in shape of magstripe (Glare, off angles, or from the Blurring)
         approx = cv2.approxPolyDP(contour, epsilon , True)                                          # Apply contour approximation and store the result in approx
 
         if len(approx ) == 4:                                                                       # If the contour has 4 sides
-            rect = cv2.minAreaRect(contour)                                                         # Creates a variable with the rectangles data
-            (x, y), (width, height), angle = rect                                                   # Assignes values from rect to get the height and width and location of the rectangle
+            (x, y), (width, height), angle = cv2.minAreaRect(contour)                               # Creates a variable with the rectangles data
             aspectRatio = min(width, height) / max(width, height)                                   # Rectangle can be flipped because image in portrait mode is in landscape and can be one side or the other
-            #test_draw_magstripe_contour(contour, filename_canny)                                   # Test drawing function
-            if aspectRatio >= .075 and aspectRatio <= .16:                                          # Has aspect ratio to look for card in diffrent angles. .1 is true demensions, but some visual distortion exsists
+            if aspectRatio >= .09 and aspectRatio <= .16:                                          # Has aspect ratio to look for card in diffrent angles. .1 is true demensions, but some visual distortion exsists
                 if height > 10 and width > 10:                                                      # If the box is bigger than 10 by 10 pixels, to filter out artifacts
-                    return (width, height)                                                          # The pixel height and width of card is returned
+                    test_draw_magstripe_contour(contour, filename_canny)                            # Test drawing function
+                    #print (aspectRatio , filename_canny)
+                    return [width, height]                                                          # The pixel height and width of card is returned
