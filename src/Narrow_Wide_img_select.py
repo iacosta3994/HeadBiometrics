ec3ca5ca4a90cd81e266d7e043b906987220bbce
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-09-01 13:38:25 -0700

    starting process of bulding ear to ear path

--- src/Narrow_Wide_img_select.py
+++ src/Narrow_Wide_img_select.py
@@ -1,6 +1,11 @@
 ''' this moduloe wil have 2 functions to find the narrowest object and the widest to determine which image to use to canny and pathfind. '''
 
-from src.canny_edge_detection_cv2 import *
+
 import cv2
+import numpy as np
+from src.image_segmentation import *
+from src.canny_edge_detection_cv2 import *
 
 def narrowest_img (filename, ):     # Inputs frame-filename to scan for the narrowest head img
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml)
