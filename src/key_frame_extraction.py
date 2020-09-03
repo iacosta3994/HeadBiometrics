96def67b6ba21bd87839d1c45ec3036f09d81069
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-09-03 14:34:28 -0700

    Checked all images extracted to array

--- src/key_frame_extraction.py
+++ src/key_frame_extraction.py
@@ -6,7 +6,7 @@ import numpy as np
 # Function splits incoming mp4 and returns
 
 
-def split_frames(cap_origin_path, video_name):
+def split_frames(cap_origin_path):
     # Assigning video from file to variable cap
     cap = cv2.VideoCapture(cap_origin_path)
     # Starting with the first{0} frame
@@ -20,9 +20,10 @@ def split_frames(cap_origin_path, video_name):
         # (Retval, image) | Grabs, decodes, and returns for next frame
         retrieved, frame = cap.read()
         if retrieved:
-            #Adds frame to list
+            # Adds frame to list
             images.append(frame)
-            #Updates frame count
+            # save_image(frame,pos_frame)
+            # Updates frame count
             pos_frame = cap.get(1)
         else:
             cap.set(1, pos_frame)
@@ -30,6 +31,7 @@ def split_frames(cap_origin_path, video_name):
 
         # if frame position is the same as the expected frame count break loop
         if cap.get(1) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+            print("frame is same as expected")
             break
 
     # Return value contains array of the individual frames
@@ -38,7 +40,7 @@ def split_frames(cap_origin_path, video_name):
     return split_frames_array
 
 
-def save_image(filename, frame):
-    filename = video_name + str(pos_frame) + '.jpeg'
+def save_image(frame, pos_frame):
+    filename = 'video_name' + str(pos_frame) + '.jpeg'
     #save_image(filename, frame)
     cv2.imwrite(filename, frame)
