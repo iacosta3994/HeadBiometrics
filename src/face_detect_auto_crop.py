73561b18f68a53d4d5d2e65e0e1897f6cd17eb76
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-09-03 05:30:36 -0700

    inputs frame modifies image to crop around face outputs image

--- src/face_detect_auto_crop.py
+++ src/face_detect_auto_crop.py
@@ -1,23 +1,24 @@
 import cv2
 import sys
 
-imagePath = sys.argv[1]
 
-image_gray = cv2.imread(imagePath, 0)
+def face_detect_auto_crop(image):
 
 
-faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-faces = faceCascade.detectMultiScale(
-    image_gray,
-    scaleFactor=1.3,
-    minNeighbors=3,
-    minSize=(30, 30)
-)
+    image_gray = cv2.imread(image, 0)
 
-print("[INFO] Found {0} Faces!".format(len(faces)))
 
-for (x, y, w, h) in faces:
-    cv2.rectangle(image_gray, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+    faces = faceCascade.detectMultiScale(
+        image_gray,
+        scaleFactor=1.3,
+        minNeighbors=3,
+        minSize=(30, 30)
+    )
 
-status = cv2.imwrite('faces_detected.jpg', image_gray)
-print("[INFO] Image faces_detected.jpg written to filesystem: ", status)
+    print("[INFO] Found {0} Faces!".format(len(faces)))
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image_gray, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    return crop_image
