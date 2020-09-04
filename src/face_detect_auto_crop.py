8cdbfdbde231b990464dccbd51066c04749cd86f
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-09-04 12:29:08 -0700

    moved function to its own py due to its versatility

--- src/face_detect_auto_crop.py
+++ src/face_detect_auto_crop.py
@@ -37,18 +37,3 @@ def face_detect_auto_crop(img, save_result):
         face_img = img[ny:ny+nr, nx:nx+nr]
         final_img = cv2.resize(face_img, (200, 200))
         return final_img
-
-
-def print_img_demensions(img):
-    height, width = img.shape[:2]
-    dimensions = img.shape
-
-    # height, width, number of channels in image
-    height = img.shape[0]
-    width = img.shape[1]
-    channels = img.shape[2]
-
-    print('Image Dimension    : ', dimensions)
-    print('Image Height       : ', height)
-    print('Image Width        : ', width)
-    print('Number of Channels : ', channels)
