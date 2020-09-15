ad9342abe2e2299fd23680213492c82c1ce53334
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-09-15 11:18:36 -0700

    made a copy to be able to return list

--- src/remove_nested.py
+++ src/remove_nested.py
@@ -1,6 +1,8 @@
 def remove_nested_with_idx(List_main, List_keep, list_main_idx):
-    for element in List_main:
+    list_to_modify = List_main.copy()
+    for element in list_to_modify:
         for i in element[list_main_idx]:
             # I is the value that is being compared to List_keep removing the element if not in main list
             if not isinstance(i, List_keep):
-                List_main.remove(element)
+                list_to_modify.remove(element)
+    return list_to_modify
