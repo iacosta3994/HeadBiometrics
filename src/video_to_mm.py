7e1b4933cc5aa319b7da8cd5b09098b502e83b02
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-09-14 20:59:18 -0700

    mag_stripe_w_h_area_ratio_magname added area changed refrence

--- src/video_to_mm.py
+++ src/video_to_mm.py
@@ -26,7 +26,7 @@ def video_to_pixel_mm(cap_origin_path):
     frame_proccessed = 0
 
     # Later this list will have all the H and W of the mag stripe
-    list_mag_stripe_w_h_ar_mn = []
+    list_mag_stripe_w_h_area_ratio_magname = []
     # Measured Magstripe 1 is 85.60 mm wide and 8.37 mm tall
     mag_stripe_constant_w_h_1 = [85.725, 8.382]
     mag_stripe_constant_w_h_2 = [85.725, 13.50155887997605]
@@ -44,29 +44,35 @@ def video_to_pixel_mm(cap_origin_path):
             # canny_image var is then used with get_magstripe_demensions
             frame_mag_stripe_w_h_ar_mn_list = get_magstripe_demensions(canny_image)
 
-            for mag_stripe_w_h_ar_mn in frame_mag_stripe_w_h_ar_mn_list:
+            for mag_stripe_w_h_area_ratio_magname in frame_mag_stripe_w_h_ar_mn_list:
                 # It checks if any width and height was in the var
-                if mag_stripe_w_h_ar_mn:
+                if mag_stripe_w_h_area_ratio_magname:
                     # Appends data into list mag stripe to be consolidated after it finishes with loop
-                    list_mag_stripe_w_h_ar_mn.append(mag_stripe_w_h_ar_mn)
+                    list_mag_stripe_w_h_area_ratio_magname.append(mag_stripe_w_h_area_ratio_magname)
                     # Ticks the counter for the next frame
             frame_proccessed += 1
 
             # Once all the frames have been processed continue below
-
+    '''
     with open('magstripe_pixel_w_h_a_n.txt', 'w') as file_txt:
-        for w_h_ar_n in list_mag_stripe_w_h_ar_mn:
+        for w_h_ar_n in list_mag_stripe_w_h_area_ratio_magname:
             file_txt.write(str(w_h_ar_n) + ' ' + '\n')
+    '''
+
+
+
+    mean_mag_stripe_area = [np.mean(list_mag_stripe_w_h_area_ratio_magname[0], axis=0)]
+
 
     '''
-    standard deviation to remove the outliers in list_mag_stripe_w_h_ar_mn using [2] as factor to remove from list or not
+    standard deviation to remove the outliers in list_mag_stripe_w_h_area_ratio_magname using [2] as factor to remove from list or not
     make sure its width height and not swapped
     '''
 
     mag_stripe_w_h = []
-    for mag_stripe_w_h_ar_mn in list_mag_stripe_w_h_ar_mn:
-        if 0.09288888888 <= mag_stripe_w_h_ar_mn[2] <= 0.10266666666:
-            mag_stripe_w_h = mag_stripe_w_h_ar_mn[:1]
+    for mag_stripe_w_h_area_ratio_magname in list_mag_stripe_w_h_area_ratio_magname:
+        if 0.09288888888 <= mag_stripe_w_h_area_ratio_magname[2] <= 0.10266666666:
+            mag_stripe_w_h = mag_stripe_w_h_area_ratio_magname[:1]
             # Creates an avg of the W and H variables
             mean_mag_stripe_w_h = (np.mean(mag_stripe_w_h, axis=0))
             # This divides measured H and W witht the avg this will have a H and W square
@@ -75,8 +81,8 @@ def video_to_pixel_mm(cap_origin_path):
             pixel_mm_mean = ((pixel_mm_w_h[0] + pixel_mm_w_h[1])/2)
             return pixel_mm_mean
 
-        elif 0.14962357464 <= mag_stripe_w_h_ar_mn[2] <= 0.1653734246:
-            mag_stripe_w_h = mag_stripe_w_h_ar_mn[:1]
+        elif 0.14962357464 <= mag_stripe_w_h_area_ratio_magname[2] <= 0.1653734246:
+            mag_stripe_w_h = mag_stripe_w_h_area_ratio_magname[:1]
             # Creates an avg of the W and H variables
             mean_mag_stripe_w_h = [np.mean(mag_stripe_w_h, axis=0)]
             # This divides measured H and W witht the avg this will have a H and W square
