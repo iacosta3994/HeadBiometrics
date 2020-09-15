fc1d3e8047d8861595ee1696a4215b8d9b7dbf4b
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-09-15 16:57:34 -0700

    no.array float type fix|Used np sub add for calculations

--- src/standard_dev_filter.py
+++ src/standard_dev_filter.py
@@ -3,16 +3,20 @@ import statistics
 
 def std_filter(list, idx): #[[number,number,number], [number, number,number]...]
     idx_pool = []
+    float_idx_pool = np.array(idx_pool, dtype=np.float32)
+
     for element in list:
         item = element[idx]
         if isinstance(item,str) == True:
             print("item in element must be an int or float")
             break
         idx_pool.append(item)
-    standard_deviation = statistics.stdev(idx_pool)
-    element_mean = [np.mean(list[idx], axis=0)]
+    float_idx_pool = np.append(float_idx_pool, idx_pool)
 
-    final_list = [x for x in idx_pool if (x > element_mean - 2 * standard_deviation)]
-    final_list = [x for x in final_list if (x < element_mean + 2 * standard_deviation)]
+    standard_deviation = statistics.stdev(idx_pool)
+    element_mean = [np.mean(float_idx_pool, axis=0)]
+    
+    final_list = [x for x in idx_pool if (x > np.subtract(element_mean, 2 * standard_deviation))]
+    final_list = [x for x in final_list if (x < np.add(element_mean, 2 * standard_deviation))]
 
     return final_list
