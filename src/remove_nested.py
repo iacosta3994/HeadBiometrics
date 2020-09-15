a05338db000fd2b68a211c2ae4f74f0f2c039c15
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-09-15 11:10:14 -0700

    destructive| removes element if nested var doesnt match list_keep var

---
+++ src/remove_nested.py
@@ -0,0 +1,6 @@
+def remove_nested_with_idx(List_main, List_keep, list_main_idx):
+    for element in List_main:
+        for i in element[list_main_idx]:
+            # I is the value that is being compared to List_keep removing the element if not in main list
+            if not isinstance(i, List_keep):
+                List_main.remove(element)
