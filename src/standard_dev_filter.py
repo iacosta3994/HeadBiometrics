e81ab2ffc6594eba62986dfd82ad36a8f2a18fb9
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-09-15 20:15:15 -0700

    2 standard deviations print function to see why bug is happening

--- src/standard_dev_filter.py
+++ src/standard_dev_filter.py
@@ -9,6 +9,7 @@ def std_filter(list, idx):  # [[number,number,number], [number, number,number]..
         item = element[idx]
         if isinstance(item, str) == True:
             print("item in element must be an int or float")
+            print(str(item))
             break
         idx_pool.append(item)
     float_idx_pool = np.append(float_idx_pool, idx_pool)
@@ -16,7 +17,7 @@ def std_filter(list, idx):  # [[number,number,number], [number, number,number]..
     standard_deviation = statistics.stdev(idx_pool)
     element_mean = [np.mean(float_idx_pool, axis=0)]
 
-    final_list = [x for x in idx_pool if (x > np.subtract(element_mean, 1 * standard_deviation))]
-    final_list = [x for x in final_list if (x < np.add(element_mean, 1 * standard_deviation))]
+    final_list = [x for x in idx_pool if (x > np.subtract(element_mean, 2 * standard_deviation))]
+    final_list = [x for x in final_list if (x < np.add(element_mean, 2 * standard_deviation))]
 
     return final_list
