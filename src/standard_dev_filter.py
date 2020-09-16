b3006c06e057f9fb994d06dce776d513de724836
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-09-16 14:47:21 -0700

    corrected float pool and allowed for function to change number of deviat

--- src/standard_dev_filter.py
+++ src/standard_dev_filter.py
@@ -3,7 +3,7 @@ import statistics
 
 def std_filter(list, idx):  # [[number,number,number], [number, number,number]...]
     idx_pool = []
-    float_idx_pool = np.array(idx_pool, dtype=np.float32)
+    float_idx_pool = np.array(idx_pool, dtype=np.float32,deviations_count=2)
 
     for element in list:
         item = element[idx]
@@ -15,10 +15,10 @@ def std_filter(list, idx):  # [[number,number,number], [number, number,number]..
             print("Must be float or int to perform standard_deviation")
     float_idx_pool = np.append(float_idx_pool, idx_pool)
 
-    standard_deviation = statistics.stdev(idx_pool)
+    standard_deviation = statistics.stdev(float_idx_pool)
     element_mean = [np.mean(float_idx_pool, axis=0)]
 
-    final_list = [x for x in idx_pool if (x > np.subtract(element_mean, 2 * standard_deviation))]
-    final_list = [x for x in final_list if (x < np.add(element_mean, 2 * standard_deviation))]
+    final_list = [x for x in idx_pool if (x > np.subtract(element_mean, deviations_count * standard_deviation))]
+    final_list = [x for x in final_list if (x < np.add(element_mean, deviations_count * standard_deviation))]
 
     return final_list
