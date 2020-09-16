50020058e81068d9137eab4a1e3832ab0ab5a9b4
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-09-15 17:36:15 -0700

    autopep8

--- src/standard_dev_filter.py
+++ src/standard_dev_filter.py
@@ -1,13 +1,13 @@
 import numpy as np
 import statistics
 
-def std_filter(list, idx): #[[number,number,number], [number, number,number]...]
+def std_filter(list, idx):  # [[number,number,number], [number, number,number]...]
     idx_pool = []
     float_idx_pool = np.array(idx_pool, dtype=np.float32)
 
     for element in list:
         item = element[idx]
-        if isinstance(item,str) == True:
+        if isinstance(item, str) == True:
             print("item in element must be an int or float")
             break
         idx_pool.append(item)
@@ -15,7 +15,7 @@ def std_filter(list, idx): #[[number,number,number], [number, number,number]...]
 
     standard_deviation = statistics.stdev(idx_pool)
     element_mean = [np.mean(float_idx_pool, axis=0)]
-    
+
     final_list = [x for x in idx_pool if (x > np.subtract(element_mean, 2 * standard_deviation))]
     final_list = [x for x in final_list if (x < np.add(element_mean, 2 * standard_deviation))]
 
