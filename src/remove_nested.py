4fa553eb5986abb00287247897ab5e91a8d422af
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-09-15 17:35:53 -0700

    corrected the need to have two for loops

--- src/remove_nested.py
+++ src/remove_nested.py
@@ -1,8 +1,10 @@
 def remove_nested_with_idx(List_main, List_keep, list_main_idx):
+    # function sistructive, making sure to copy
     list_to_modify = List_main.copy()
-    for element in list_to_modify:
-        for i in element[list_main_idx]:
-            # I is the value that is being compared to List_keep removing the element if not in main list
-            if not isinstance(i, List_keep):
-                list_to_modify.remove(element)
+    # runs through each element in specific idx and compares it to specific list
+    for elem_in_list in List_main:
+        item = elem_in_list[list_main_idx]
+        # checks item to see if value is in List_keep
+        if item not in List_keep:
+            list_to_modify.remove(elem_in_list)
     return list_to_modify
