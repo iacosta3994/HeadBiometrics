56701a9ef628961796edbe161e9ebcc4a1163fbd
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-09-15 17:44:16 -0700

    using one standard deviation

--- src/standard_dev_filter.py
+++ src/standard_dev_filter.py
@@ -16,7 +16,7 @@ def std_filter(list, idx):  # [[number,number,number], [number, number,number]..
     standard_deviation = statistics.stdev(idx_pool)
     element_mean = [np.mean(float_idx_pool, axis=0)]
 
-    final_list = [x for x in idx_pool if (x > np.subtract(element_mean, 2 * standard_deviation))]
-    final_list = [x for x in final_list if (x < np.add(element_mean, 2 * standard_deviation))]
+    final_list = [x for x in idx_pool if (x > np.subtract(element_mean, 1 * standard_deviation))]
+    final_list = [x for x in final_list if (x < np.add(element_mean, 1 * standard_deviation))]
 
     return final_list
