ee70241303d5d5c42e4d0536fa3a02166f9da3ee
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-09-22 13:24:44 -0700

    finds largest contour (head) and prints outline to test

--- src/Face_contour_width.py
+++ src/Face_contour_width.py
@@ -1,4 +1,17 @@
+import os
 import cv2
+import uuid
+from src.canny_edge_detection_cv2 import *
 
 def segment_img(img):
-    
+    img_canny = make_canny_face(img)
+
+    cnts = cv2.findContours(img_canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    c = max(cnts, key=cv2.contourArea)
+
+    cv2.drawContours(img_canny, c, -1, (0, 255, 0), 3)
+
+    path = 'E:/test'
+    contour_name = str(uuid.uuid4())
+    cv2.imwrite(os.path.join(path , 'contour_name'+'.jpg'), img_canny)
