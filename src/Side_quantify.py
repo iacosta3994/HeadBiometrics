fe45d7e8dc4fb78aa0c3bd6eee5803755c9793ab
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-09-22 19:21:54 -0700

    added function to retrieve largest contour in img

--- src/Side_quantify.py
+++ src/Side_quantify.py
@@ -8,7 +8,12 @@ from src.key_frame_extraction import *
 from src.canny_edge_detection_cv2 import *
 from src.mag_stripe_search import *
 from src.video_to_mm import *
+from src.face_contour_width import *
 
+path = 'Video_Tests\A_test_Sanawar.mp4'
 img_array = split_frames(path)
 pixel_mm = video_to_pixel_mm(img_array)
+largest_contour = largest_contour(img_array)
+contour, area, img = largest_contour[0][0], largest_contour[0][1], largest_contour[0][2]
+
 print(pixel_mm)
