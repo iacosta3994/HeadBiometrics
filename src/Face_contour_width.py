93f2faed399f157ab70e581fb9518f4735fce8ea
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-09-22 19:22:49 -0700

    inputs array || outputs [contour, area, img]

--- src/Face_contour_width.py
+++ src/Face_contour_width.py
@@ -3,15 +3,44 @@ import cv2
 import uuid
 from src.canny_edge_detection_cv2 import *
 
-def segment_img(img):
+
+
+def largest_contour(img_array):
+    largest_contour = []
+    for img in img_array:
+        contour, img_canny = head_contour(img)
+        area = cv2.contourArea(contour)
+        if not largest_contour:
+            largest_contour.append([contour, area, img])
+            continue
+        if area > largest_contour[0][1]:
+            largest_contour.pop(0)
+            largest_contour.append([contour, area, img])
+
+            cv2.drawContours(img_canny, contour, -1, (0, 255, 0), 3)
+            path = 'E:/test'
+            contour_name = str(uuid.uuid4())
+            cv2.imwrite(os.path.join(path , contour_name + '.jpg'), img_canny)
+        else:
+            continue
+
+
+    return largest_contour
+
+def head_contour(img):
     img_canny = make_canny_face(img)
 
-    cnts = cv2.findContours(img_canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
-    c = max(cnts, key=cv2.contourArea)
+    contour = cv2.findContours(img_canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contour = get_contour(contour)
+    contour = max(contour, key=cv2.contourArea)
 
-    cv2.drawContours(img_canny, c, -1, (0, 255, 0), 3)
+    return contour, img_canny
 
-    path = 'E:/test'
-    contour_name = str(uuid.uuid4())
-    cv2.imwrite(os.path.join(path , 'contour_name'+'.jpg'), img_canny)
+def get_contour(contour):
+    if len(contour) == 2:
+        contour = contour[0]
+    elif len(contour) == 3:
+        contour = contour[1]
+    else:
+        raise Exception(("contour in face_contour_width is not working as expected,findContours changed output type check opencv documentation for updates"))
+    return contour
