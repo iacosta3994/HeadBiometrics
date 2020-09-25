8933e742f4ff00f5a759ced8b9be0f3475fc5795
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-09-24 20:48:48 -0700

    tuened it into sobel edge detector

--- src/canny_edge_detection_cv2.py
+++ src/canny_edge_detection_cv2.py
@@ -3,8 +3,7 @@ import cv2
 
 # Function will load frame from file_path and will save canny edge image in same directory with new name
 def make_canny_magstripe(img):
-
-    gray_img = cv2.cvtColor(np.float32(img), cv2.COLOR_BGR2GRAY)
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # Blurring to reduce artifacts
     blurred_img = cv2.GaussianBlur(gray_img, (7, 7), 0)
     clahe = cv2.createCLAHE()  # Contrast Limited Adaptive Histogram Equalization
@@ -22,11 +21,28 @@ def make_canny_magstripe(img):
     return img_canny_dilation
 
 
-def make_canny_face(img):
-    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    blurred_img = cv2.GaussianBlur(gray_img, (5, 5), 0)
-    img_canny = auto_canny(blurred_img)
-    return img_canny
+def make_sobel_face(img):
+    scale = 1
+    delta = 0
+    ddepth = cv2.CV_16S
+    if len(img.shape) != 2:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    blurred_img = cv2.GaussianBlur(img, (3, 3), 0)
+
+    grad_x = cv2.Sobel(blurred_img, ddepth, 1, 0, ksize=3, scale= scale, delta= delta, borderType=cv2.BORDER_DEFAULT)
+    grad_y = cv2.Sobel(blurred_img, ddepth, 0, 1, ksize=3, scale= scale, delta= delta, borderType=cv2.BORDER_DEFAULT)
+
+    abs_grad_x = cv2.convertScaleAbs(grad_x)
+    abs_grad_y = cv2.convertScaleAbs(grad_y)
+    grad = cv2.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
+
+    kernel = np.ones((3, 3), np.uint8)
+    img_erosion = cv2.erode(grad, kernel, iterations=1)
+    img_canny_dilation = cv2.dilate(img_erosion, kernel, iterations=2)
+
+
+    return img_canny_dilation
 
 
 # Runs an automated version of canny with optimal lower and upper thresholds || sigma .33 Default
