128ce9a8f0b41f9dbd4757a277a504ef8904166a
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-09-25 21:55:26 -0700

    made unique autocanny for head edge detection, makes sobel detection

--- src/canny_edge_detection_cv2.py
+++ src/canny_edge_detection_cv2.py
@@ -2,6 +2,8 @@ import numpy as np
 import cv2
 
 # Function will load frame from file_path and will save canny edge image in same directory with new name
+
+
 def make_canny_magstripe(img):
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # Blurring to reduce artifacts
@@ -20,32 +22,9 @@ def make_canny_magstripe(img):
     #cv2.imwrite('filename.png', img_canny_dilation)
     return img_canny_dilation
 
-
-def make_sobel_face(img):
-    scale = 1
-    delta = 0
-    ddepth = cv2.CV_16S
-    if len(img.shape) != 2:
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    blurred_img = cv2.GaussianBlur(img, (3, 3), 0)
-
-    grad_x = cv2.Sobel(blurred_img, ddepth, 1, 0, ksize=3, scale= scale, delta= delta, borderType=cv2.BORDER_DEFAULT)
-    grad_y = cv2.Sobel(blurred_img, ddepth, 0, 1, ksize=3, scale= scale, delta= delta, borderType=cv2.BORDER_DEFAULT)
-
-    abs_grad_x = cv2.convertScaleAbs(grad_x)
-    abs_grad_y = cv2.convertScaleAbs(grad_y)
-    grad = cv2.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
-
-    kernel = np.ones((3, 3), np.uint8)
-    img_erosion = cv2.erode(grad, kernel, iterations=1)
-    img_canny_dilation = cv2.dilate(img_erosion, kernel, iterations=2)
-
-
-    return img_canny_dilation
-
-
 # Runs an automated version of canny with optimal lower and upper thresholds || sigma .33 Default
+
+
 def auto_canny(img, sigma=0.33):
     # Gets median value of channel intensity | unique for each image
     v = np.median(img)
@@ -56,3 +35,49 @@ def auto_canny(img, sigma=0.33):
     # Img_canny now has the canny edge image
     img_canny = cv2.Canny(img, lower, upper)
     return img_canny
+
+    # faces uses sobel for the edge detection along the x and y axis and combines the img
+
+
+def make_sobel_face(img):
+    scale = 1
+    delta = 0
+    ddepth = cv2.CV_16S
+    # makes sure that img is already binary, if its a color photo it will turn it binary
+    if len(img.shape) != 2:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # makes the gradient using x and y axis
+    grad_x = cv2.Sobel(img, ddepth, 1, 0, ksize=5, scale=scale,
+                       delta=delta, borderType=cv2.BORDER_DEFAULT)
+    grad_y = cv2.Sobel(img, ddepth, 0, 1, ksize=5, scale=scale,
+                       delta=delta, borderType=cv2.BORDER_DEFAULT)
+    # this section prepares it to combine together
+    abs_grad_x = cv2.convertScaleAbs(grad_x)
+    abs_grad_y = cv2.convertScaleAbs(grad_y)
+    img = cv2.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
+    # the white pixels are expanded to company any contours that are seperated
+    img = cv2.dilate(img, (5, 5))
+    # runs another level of dilation and then erodes it
+    img = cv2.morphologyEx(img, cv2.MORPH_OPEN, (3, 3))
+    return img
+
+
+def auto_canny_face(img, sigma=0.05):
+
+    img = cv2.GaussianBlur(img, (5, 5), 0)
+    # Gets median value of channel intensity | unique for each image
+    v = np.median(img)
+    # Sets lower int valeue for canny function
+    lower = int(max(0, (1.0 - sigma) * v))
+    # Sets higher int value for canny function
+    upper = int(min(255, (1.0 + sigma) * v))
+
+    # Img_canny now has the canny edge image
+    img = cv2.Canny(img, lower, upper)
+
+    img = cv2.dilate(img, (3, 3))
+    img = cv2.morphologyEx(img, cv2.MORPH_OPEN, (7, 7))
+    img = cv2.erode(img, (7, 7))
+
+    return img
