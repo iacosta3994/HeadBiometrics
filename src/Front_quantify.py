a98f3fba2bb91404577d20113c6f7498fc3202cb
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-10-09 12:29:10 -0700

    addidng print test functions to see what is going wrong

--- src/Front_quantify.py
+++ src/Front_quantify.py
@@ -5,22 +5,23 @@ import numpy as np
 import matplotlib
 import cv2
 from src.key_frame_extraction import *
-from src.canny_edge_detection_cv2 import *
-from src.mag_stripe_search import *
 from src.video_to_mm import *
-from src.face_contour_width import *
 from src.narrow_wide_img_select import narrowest_img
 from src.path_finder import *
 from src.face_detect_auto_crop import crop_above_eyes
+from src.face_contour_width import img_head_contour
 
 
-path = 'Video_Tests\A_test_self.mp4'
+path = 'Video_Tests\A_test_alex.mp4'
 img_array = split_frames(path)
 pixel_mm, mag_xy = video_to_pixel_mm(img_array)
+print(pixel_mm, mag_xy)
 narrow_head_img = narrowest_img(img_array)
+if narrow_head_img is None:
+    print("Narrow head img is none")
 above_eyes = crop_above_eyes(narrow_head_img,mag_xy)
 if above_eyes is None:
-    print("Can't open above eyes file")
+    print("above eyes is none")
 
 front_contour = img_head_contour(above_eyes)
 
@@ -32,8 +33,6 @@ front_contour = img_head_contour(above_eyes)
 #contour_mask = make_mask_from_contour(front_contour)
 
 #astar(contour_mask, contour_start, contour_end, allow_diagonal_movement=True)
-
-print(pixel_mm)
 
 
 contour_name = str(uuid.uuid4())
