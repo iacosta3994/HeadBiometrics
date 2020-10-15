f39adc90555f50b0799558980bb3c6ffb929e06e
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-10-15 16:44:41 -0700

    function that returns the largest w or h of a contour

---
+++ src/contour_width_max.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+
+def ret_contour_width(contour):
+    box = cv2.minAreaRect(contour)
+    box = cv2.boxPoints(box)
+    box = np.int0(box)
+    (tl,tr,br,bl) = box
+    left_max = tl - bl
+    right_max = tr - br
+    if left_max[0] >= right_max[0]:
+        (x,y)=left_max
+        if x > y:
+            return x
+        else:
+            return y
+    else:
+        (x,y) = right_max
+        if x > y:
+            return x
+        else:
+            return y
