e54f155d4275e095e9c3fac4b314e84b038e7fac
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-10-15 16:45:07 -0700

    added ear to ear metric and head breadth

--- src/Front_quantify.py
+++ src/Front_quantify.py
@@ -10,6 +10,7 @@ from src.narrow_wide_img_select import narrowest_img
 from src.path_finder import *
 from src.face_detect_auto_crop import crop_above_eyes
 from src.face_contour_width import img_head_contour
+from src.contour_width_max import ret_contour_width
 
 
 path = 'Video_Tests\A_test_alex.mp4'
@@ -26,6 +27,14 @@ else:
         print("img_samples_array is none")
     else:
         main_canny, main_contour, main_contour_length = img_head_contour(img_samples_array)
+
+
+
+        ear_to_ear_mm = int(main_contour_length * pixel_mm[0])
+
+        head_width = ret_contour_width(main_contour)
+        print(head_width)
+
 cv2.imwrite("canny_image.jpg", main_canny)
 
 #contour_start = contour_bottom_left(front_contour)
