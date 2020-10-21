1e7368f7446304e46f133d2685bf4c8b4289a630
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-10-20 18:21:31 -0700

    point return used to crop and find length

--- src/Side_quantify.py
+++ src/Side_quantify.py
@@ -5,8 +5,12 @@ import matplotlib
 import cv2
 from src.key_frame_extraction import split_frames
 from src.video_to_mm import video_to_pixel_mm
+from src.narrow_wide_img_select import widest_img
+from src.xy_img_points_click import point_return
 from src.keep_above_points import keep_img_above_points
+from src.face_contour_width import img_head_contour
 from src.distance_between_points import dif_in_points
+
 
 def side_mm_metrics(path):
     img_array = split_frames(path)
@@ -16,9 +20,11 @@ def side_mm_metrics(path):
     if side_head_img is None:
         print("side_head_img is None")
     else:
-        pointA, pointB =
-        final_img = keep_img_above_points(side_head_img, pointA, pointB)
-        front2nape = img_mask_contour(final_img)
+        front, nape = point_return(side_head_img)
+        final_img = keep_img_above_points(side_head_img.copy, front, nape)
+        _, _, front2nape = img_head_contour(final_img)
+
+        pointA, pointB = point_return(side_head_img)
         length = dif_in_points(pointA, pointB)
 
 
