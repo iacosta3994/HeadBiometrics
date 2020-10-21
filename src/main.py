3311804682d0f788867b4bf8c16d3f93309b1ed6
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-10-20 18:55:30 -0700

    main functin to present the measurements

---
+++ src/main.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import numpy as np
+
+
+from src.front_quantify import  front_mm_metrics
+from src.side_quantify import  side_mm_metrics
+
+def main(f_path, s_path):
+    ear_to_ear_mm , head_width_mm = front_mm_metrics(f_path)
+    front2nape_mm, length_mm = side_mm_metrics(s_path)
+
+    circumference_mm = (((width * 2) + (length *2))* 0.834626841674)
+
+    return ear_to_ear_mm, front2nape_mm, circumference_mm
+
+
+ear_to_ear_mm, front2nape_mm, circumference_mm = main('Video_Tests/A_test_self.mp4','Video_Tests/B_test_self.mp4')
