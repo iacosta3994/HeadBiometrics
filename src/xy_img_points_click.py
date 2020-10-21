55f7eb035ee41af7ec147f3f02369dadf3e9a574
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-10-20 17:21:35 -0700

    creating function to return x y of points selected

---
+++ src/xy_img_points_click.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+from side_quantify import  side_head_img
+
+def point_select(event, x, y, flags, params):
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print(x, ' ', y)
+
+if __name__ == "__main__":
+    img = side_head_img
+    cv2.imshow("img", img)
+    cv2.setMouseCallback("img", point_select)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
