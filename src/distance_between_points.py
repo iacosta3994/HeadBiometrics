eb61a18cba1a2de54fdfc234f5a2433ce77f3e67
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-10-28 22:17:17 -0700

    we imported numpy might as well use it

--- src/distance_between_points.py
+++ src/distance_between_points.py
@@ -4,4 +4,4 @@ import numpy as np
 def dis_in_points(pointA, pointB):
     (xA, yA) = pointA
     (xB, yB) = pointB
-    return round(sqrt((xA-xB)**2 + (yA - yB)**2))
+    return round(np.sqrt((xA-xB)**2 + (yA - yB)**2))
