f385aa0dde3436e81bbb35fe0e5c414de85d2c86
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-10-28 22:16:54 -0700

    angled mask between 2 points, still needs work to merge mask W/ original

--- src/keep_above_points.py
+++ src/keep_above_points.py
@@ -4,17 +4,31 @@ import numpy as np
 
 def keep_img_above_points(image, pointA, pointB):
 
+    pA1, pA2 = pointA
+    pB1, pB2 = pointB
     # slope of line
-    m = float(pointA[1] - pointB[1]) / float(pointA[0] - pointB[0])
-    c = pointB[1] - m*pointB[0]
 
-    y, x, _ = image.shape
-    mask = np.zeros((y, x), np.uint8)
+    m = float(pB2 - pA2) / float(pB1 - pA1)
+    c = pB2 - m*pB1
 
-    for x in np.arange(0, pointA[0]+1):
-        for y in np.arange(0, pointB[1] + 1):
-            if y < m*x + c:
-                mask[y][x] = (255, 255, 255)
-    final_img = cv2.merge((image[:, :, 0], img[:, :, 1], img[:, :, 2], mask[:, :, 0]))
+    if image is None:
+        print("image is None_ cant continue 'keep img above points.py'")
 
+    (imgy, imgx, imgc) = image.shape
+    mask = np.zeros((imgy, imgx, imgc), np.uint8)
+
+    for y in np.arange(0, pB2 + 1):
+        for x in np.arange(0, pA1+1):
+
+            if y > m*x + c:
+
+                mask[y,x] = [255, 255, 255]
+
+
+    final_img = cv2.merge((image[:, :, 0], image[:, :, 1], image[:, :, 2], mask[:, :, 0]))
+    while True:
+        cv2.imshow('final_img', final_img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cv2.destroyAllWindows()
     return final_img
