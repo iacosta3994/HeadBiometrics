0d26ca87ad534f58ec3f3c3fb0276adce226b131
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-11-02 13:51:02 -0800

    selected points finds the slope and blocks out underneath image.

--- src/keep_above_points.py
+++ src/keep_above_points.py
@@ -1,34 +1,38 @@
 import cv2
 import numpy as np
+from src.canny_edge_detection_cv2 import auto_canny_face
 
 
 def keep_img_above_points(image, pointA, pointB):
 
-    pA1, pA2 = pointA
-    pB1, pB2 = pointB
+    x1, y1 = pointA
+    x2, y2 = pointB
     # slope of line
 
-    m = float(pB2 - pA2) / float(pB1 - pA1)
-    c = pB2 - m*pB1
+    m = float(y2 - y1) / float(x2 - x1)
+    c = y2 - m*x2
 
     if image is None:
         print("image is None_ cant continue 'keep img above points.py'")
 
     (imgy, imgx, imgc) = image.shape
-    mask = np.zeros((imgy, imgx, imgc), np.uint8)
+    #mask = np.zeros((imgy, imgx, imgc), np.uint8)
+    img_canny = auto_canny_face(image)
+    img_canny = cv2.cvtColor(img_canny, cv2.COLOR_GRAY2BGR)
+    for x in np.arange(x1, imgx):
+        for y in np.arange(0, imgy):
+            if y < m*x + c:
 
-    for y in np.arange(0, pB2 + 1):
-        for x in np.arange(0, pA1+1):
-
-            if y > m*x + c:
-
-                mask[y,x] = [255, 255, 255]
+                img_canny[y][x] = [0, 0, 0]
 
 
-    final_img = cv2.merge((image[:, :, 0], image[:, :, 1], image[:, :, 2], mask[:, :, 0]))
-    while True:
-        cv2.imshow('final_img', final_img)
+
+    '''while True:
+        cv2.imshow('final_img', img_canny)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     cv2.destroyAllWindows()
-    return final_img
+    cv2.imwrite('mask.jpg', mask)
+    cv2.imwrite('img_canny.jpg', img_canny)
+    cv2.imwrite('image.jpg', image)'''
+    return img_canny
