a0878640923977cf1a4f8077f2ff21bc2225f264
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-11-06 14:29:46 -0800

    gets the bounding box of contour and returns the width

--- src/contour_width_max.py
+++ src/contour_width_max.py
@@ -3,21 +3,24 @@ import numpy as np
 
 
 def ret_contour_width(contour):
+    # Builds a box around the inputed contour
     box = cv2.minAreaRect(contour)
+    # collects data from box
     box = cv2.boxPoints(box)
+    # changes format to 64 bit integer
     box = np.int0(box)
+    # top Left ... bottom Left
     (tl, tr, br, bl) = box
-    left_max = tl - bl
-    right_max = tr - br
-    if left_max[0] >= right_max[0]:
-        (x, y) = left_max
-        if x > y:
-            return x
-        else:
-            return y
-    else:
-        (x, y) = right_max
-        if x > y:
-            return x
-        else:
-            return y
+
+    left_max = abs(tl - bl)
+    right_max = abs(tr - br)
+    top_max = abs(tl - tr)
+    bottom_max = abs(bl - br)
+
+    max_list = []
+    max_list.append(max(left_max))
+    max_list.append(max(right_max))
+    max_list.append(max(top_max))
+    max_list.append(max(bottom_max))
+
+    return max(max_list)
