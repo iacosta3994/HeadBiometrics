eaaf36756ae537cdab67b6267746ba10a6e91fc8
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-11-06 18:30:16 -0800

    changed to refrence import functino with recursion

--- src/Front_quantify.py
+++ src/Front_quantify.py
@@ -6,7 +6,7 @@ import cv2
 from src.key_frame_extraction import split_frames
 from src.video_to_mm import video_to_pixel_mm
 from src.narrow_wide_img_select import narrowest_img
-from src.face_detect_auto_crop import crop_above_eyes
+from src.face_detect_auto_crop import crop_above_eyes_w_recursion
 from src.face_contour_width import img_head_contour
 from src.contour_width_max import ret_contour_width
 
@@ -20,7 +20,7 @@ def front_mm_metrics(path):
     if narrow_head_img is None:
         print("Narrow head img is none")
     else:
-        img_samples_array = crop_above_eyes(narrow_head_img, mag_xy)
+        img_samples_array = crop_above_eyes_w_recursion(narrow_head_img, mag_xy)
         if img_samples_array is None:
             print("img_samples_array is none")
         else:
