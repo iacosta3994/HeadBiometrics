93f947c9a9ba70af39c47322cf67fc1f53fdffe8
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-11-11 17:24:46 -0800

    using Proctoring AI to select widest img and narrow

--- src/Narrow_Wide_img_select.py
+++ src/Narrow_Wide_img_select.py
@@ -1,115 +1,110 @@
+import sys
 import cv2
-import numpy as np
-import types
-from src.face_contour_width import head_contour
 
-left_eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_lefteye_2splits.xml')
-right_eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_righteye_2splits.xml')
-eye_pair = cv2.CascadeClassifier(cv2.data.haarcascades + 'frontalEyes35x16.xml')
+from src.Proctoring_AI.face_detector import get_face_detector, find_faces
+from src.Proctoring_AI.face_landmarks import get_landmark_model, detect_marks
+from src.Proctoring_AI.head_pose_estimation import *
 
-def narrowest_img(img_array):     # Inputs frame-filename to scan for the narrowest head img
 
-    # establishing var outside loop
-    dist = None
-    ret_img = None
 
-    # itteration for each img
+
+def narrowest_img(img_array):
+
+    face_model = get_face_detector()
+    landmark_model = get_landmark_model()
+
+    size = img_array[0].shape
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    # 3D model points.
+    model_points = np.array([
+                                (0.0, 0.0, 0.0),             # Nose tip
+                                (0.0, -330.0, -65.0),        # Chin
+                                (-225.0, 170.0, -135.0),     # Left eye left corner
+                                (225.0, 170.0, -135.0),      # Right eye right corne
+                                (-150.0, -150.0, -125.0),    # Left Mouth corner
+                                (150.0, -150.0, -125.0)      # Right mouth corner
+                            ])
+
+    # Camera internals
+    focal_length = size[1]
+    center = (size[1]/2, size[0]/2)
+    camera_matrix = np.array(
+                             [[focal_length, 0, center[0]],
+                             [0, focal_length, center[1]],
+                             [0, 0, 1]], dtype = "double"
+                             )
     for img in img_array:
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        left_eye = left_eye_cascade.detectMultiScale(img, 1.05, 4)
-        right_eye = right_eye_cascade.detectMultiScale(img, 1.05, 4)
-        both_eyes = eye_pair.detectMultiScale(img, 1.05, 4)
 
-        # if both eyes found
-        if (type(left_eye).__module__ == np.__name__) and (type(right_eye).__module__ == np.__name__):
-
-            # itterating through both combos of left and right
-            for (bex, bey, bew, beh)  in both_eyes:
-                for (lex, ley, lew, leh) in left_eye:
-
-                    for (rex, rey, rew, reh) in right_eye:
-
-
-
-
-                        test_img = cv2.rectangle(img, (lex, ley), (lex + lew, ley +leh), (255,0,0), 2)
-                        cv2.imshow('left eye', test_img)
-                        cv2.waitKey(0)
-                        cv2.destroyAllWindows()
-                        test_img = cv2.rectangle(test_img, (rex, rey), (rex + rew, rey +reh), (0,255,0), 2)
-                        cv2.imshow('right eye', test_img)
-                        cv2.waitKey(0)
-                        cv2.destroyAllWindows()
-                        test_img = cv2.rectangle(test_img, (bex, bey), (bex + bew, bey +beh), (0,0,255), 2)
-                        cv2.imshow('Both eyes with both eyes', test_img)
-                        cv2.waitKey(0)
-                        cv2.destroyAllWindows()
-
-                        # distance between points of detected eyes
-                        img_dist = np.sqrt((rex-lex)**2 + (rey - ley)**2)
-
-                        # if dist not established first img creates value
-                        if dist == None:
-
-                            dist = img_dist
-                            ret_img = img
-                        # when larger distance detected updates return img
-                        if img_dist < dist:
-                            dist = img_dist
-                            ret_img = img
+        faces = find_faces(img, face_model)
+        for face in faces:
+            marks = detect_marks(img, landmark_model, face)
+            # mark_detector.draw_marks(img, marks, color=(0, 255, 0))
+            image_points = np.array([
+                                    marks[30],     # Nose tip
+                                    marks[8],     # Chin
+                                    marks[36],     # Left eye left corner
+                                    marks[45],     # Right eye right corne
+                                    marks[48],     # Left Mouth corner
+                                    marks[54]      # Right mouth corner
+                                ], dtype="double")
+            dist_coeffs = np.zeros((4,1)) # Assuming no lens distortion
+            (success, rotation_vector, translation_vector) = cv2.solvePnP(model_points, image_points, camera_matrix, dist_coeffs, flags=cv2.SOLVEPNP_UPNP)
 
 
+            # Project a 3D point (0, 0, 1000.0) onto the image plane.
+            # We use this to draw a line sticking out of the nose
+
+            (nose_end_point2D, jacobian) = cv2.projectPoints(np.array([(0.0, 0.0, 1000.0)]), rotation_vector, translation_vector, camera_matrix, dist_coeffs)
+
+            for p in image_points:
+                cv2.circle(img, (int(p[0]), int(p[1])), 3, (0,0,255), -1)
 
 
-    if ret_img is not None:
-        return ret_img
+            p1 = ( int(image_points[0][0]), int(image_points[0][1]))
+            p2 = ( int(nose_end_point2D[0][0][0]), int(nose_end_point2D[0][0][1]))
+            x1, x2 = head_pose_points(img, rotation_vector, translation_vector, camera_matrix)
+
+            cv2.line(img, p1, p2, (0, 255, 255), 2)
+            cv2.line(img, tuple(x1), tuple(x2), (255, 255, 0), 2)
+
+            try:
+                m = (p2[1] - p1[1])/(p2[0] - p1[0])
+                ang1 = int(math.degrees(math.atan(m)))
+            except:
+                ang1 = 90
+
+            try:
+                m = (x2[1] - x1[1])/(x2[0] - x1[0])
+                ang2 = int(math.degrees(math.atan(-1/m)))
+            except:
+                ang2 = 90
+
+                # print('div by zero error')
+            if ang1 >= 48:
+                print('Head down')
+                cv2.putText(img, 'Head down', (30, 30), font, 2, (255, 255, 128), 3)
+            elif ang1 <= -48:
+                print('Head up')
+                cv2.putText(img, 'Head up', (30, 30), font, 2, (255, 255, 128), 3)
+
+            if ang2 >= 48:
+                print('Head right')
+                cv2.putText(img, 'Head right', (90, 30), font, 2, (255, 255, 128), 3)
+            elif ang2 <= -48:
+                print('Head left')
+                cv2.putText(img, 'Head left', (90, 30), font, 2, (255, 255, 128), 3)
+
+            cv2.putText(img, str(ang1), tuple(p1), font, 2, (128, 255, 255), 3)
+            cv2.putText(img, str(ang2), tuple(x1), font, 2, (255, 255, 128), 3)
+        cv2.imshow('img', img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cv2.destroyAllWindows()
+
 
 
 
 
 def widest_img(img_array, img_orientation):
-
-    widest_head_img = None
-    largest_contour = None
-    # itterating through each image in img array
-    for img in img_array:
-        # runs head contour that gets the contour from the img
-        for orientation in img_orientation:
-            beginY, endY, beginX, endX = orientation
-            og_img = img.copy()
-            img = img[beginY:endY, beginX:endX]
-            contour, img_canny = head_contour(img)
-
-            contour_length = cv2.arcLength(contour, closed=False)
-            if largest_contour == None or contour_length > largest_contour:
-                cv2.drawContours(img, contour, -1, (0,255,0), 3)
-                cv2.imshow('Widest img', img_canny)
-                cv2.waitKey(0)
-                cv2.destroyAllWindows()
-                largest_contour = contour_length
-                widest_head_img = og_img
-            else:
-                continue
-
-    return widest_head_img
-
-
-def front_head_select(img_array):
-    modelFile = "src/tensor_pose_estimation/res10_300x300_ssd_iter_140000.caffemodel"
-    configFile = "src/tensor_pose_estimation/deploy.prototxt.txt"
-    net = cv2.dnn.readNetFromCaffe(configFile, modelFile)
-    for img in img_array:
-        img = cv2.imread('test.jpg')
-        h, w = img.shape[:2]
-        og_img = img.copy()
-        blob = cv2.dnn.blobFromImage(cv2.resize(og_img, (300, 300)), 1.0, (300, 300), (104.0, 117.0, 123.0))
-        net.setInput(blob)
-        faces = net.forward()
-    #to draw faces on image
-        for i in range(faces.shape[2]):
-                confidence = faces[0, 0, i, 2]
-                if confidence > 0.5:
-                    box = faces[0, 0, i, 3:7] * np.array([w, h, w, h])
-                    (x, y, x1, y1) = box.astype("int")
-                    cv2.rectangle(img, (x, y), (x1, y1), (0, 0, 255), 2)
-                    
+    pass
