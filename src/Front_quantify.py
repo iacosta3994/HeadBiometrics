6671e76ed4b920b2028884288b1849ca0aab564a
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-11-13 13:45:24 -0800

    moved crop^eye to narrow select, still testing with code

--- src/Front_quantify.py
+++ src/Front_quantify.py
@@ -6,7 +6,7 @@ import cv2
 from src.key_frame_extraction import split_frames
 from src.video_to_mm import video_to_pixel_mm
 from src.narrow_wide_img_select import narrowest_img
-from src.face_detect_auto_crop import crop_above_eyes
+
 from src.face_contour_width import img_head_contour
 from src.contour_width_max import ret_contour_width
 
@@ -14,23 +14,21 @@ from src.contour_width_max import ret_contour_width
 def front_mm_metrics(path):
     img_array = split_frames(path)
     pixel_mm = video_to_pixel_mm(img_array)
+    
 
-    narrow_head_img, fin_img_eyes_xy = narrowest_img(img_array)
+    narrow_head_img = narrowest_img(img_array)
 
-    if narrow_head_img is None:
-        print("Narrow head img is none")
-    else:
-        img_samples_array = crop_above_eyes(narrow_head_img, fin_img_eyes_xy)
 
-        if img_samples_array is None:
-            print("img_samples_array is none")
-        else:
-            main_canny, main_contour, main_contour_length = img_head_contour(img_samples_array)
+    cv2.imshow('narrow_head_img=', narrow_head_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-            ear_to_ear_mm = int(main_contour_length * pixel_mm[0])
+    main_contour, main_contour_length = img_head_contour(narrow_head_img)
 
-            head_width_mm = int(ret_contour_width(main_contour) * pixel_mm[0])
+    ear_to_ear_mm = int(main_contour_length * pixel_mm[0])
 
-            # cv2.imwrite("results.jpg",narrow_head_img)
-            #cv2.imwrite("canny_image.jpg", main_canny)
+    head_width_mm = int(ret_contour_width(main_contour) * pixel_mm[0])
+
+    # cv2.imwrite("results.jpg",narrow_head_img)
+    #cv2.imwrite("canny_image.jpg", main_canny)
     return ear_to_ear_mm, head_width_mm
