a24bbe853bfe543a421de5d6473ec550b6af21c3
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-11-17 23:23:34 -0800

    image to img, corrected for portrait, still working it out no final ret?

--- src/keep_above_points.py
+++ src/keep_above_points.py
@@ -1,9 +1,10 @@
 import cv2
+import argparse
 import numpy as np
-from src.canny_edge_detection_cv2 import auto_canny_face
+from src.canny_edge_detection_cv2 import neural_edge_detection
 
 
-def keep_img_above_points(image, pointA, pointB):
+def keep_img_above_points(img, pointA, pointB):
 
     x1, y1 = pointA
     x2, y2 = pointB
@@ -12,27 +13,27 @@ def keep_img_above_points(image, pointA, pointB):
     m = float(y2 - y1) / float(x2 - x1)
     c = y2 - m*x2
 
-    if image is None:
-        print("image is None_ cant continue 'keep img above points.py'")
+    if img is None:
+        print("img is None_ cant continue 'keep img above points.py'")
 
-    (imgy, imgx, imgc) = image.shape
-    #mask = np.zeros((imgy, imgx, imgc), np.uint8)
-    img_canny = auto_canny_face(image)
-    img_canny = cv2.cvtColor(img_canny, cv2.COLOR_GRAY2BGR)
-    for x in np.arange(x1, imgx):
-        for y in np.arange(0, imgy):
-            if y < m*x + c:
+    (imgy, imgx, imgc) = img.shape
+
+    img_canny = neural_edge_detection(img, ret_contour = False)
+
+    if len(img.shape) == 2:
+        img_canny = cv2.cvtColor(img_canny, cv2.COLOR_GRAY2BGR)
+
+
+    for x in np.arange(0, imgx):
+        for y in np.arange(min(y1,y2), imgy):
+
+            if y > m*x + c:
 
                 img_canny[y][x] = [0, 0, 0]
 
-
-
-    '''while True:
-        cv2.imshow('final_img', img_canny)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    cv2.imshow('keep_above_points', img_canny)
+    cv2.waitKey(0)
     cv2.destroyAllWindows()
-    cv2.imwrite('mask.jpg', mask)
-    cv2.imwrite('img_canny.jpg', img_canny)
-    cv2.imwrite('image.jpg', image)'''
+
+
     return img_canny
