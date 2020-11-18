c1260d44cbaf5d66b64f63f10acd809a21c550db
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-11-17 23:20:38 -0800

    added prints to find issue, issue in point select

--- src/Side_quantify.py
+++ src/Side_quantify.py
@@ -7,7 +7,6 @@ import copy
 from src.key_frame_extraction import split_frames
 from src.video_to_mm import video_to_pixel_mm
 from src.narrow_wide_img_select import widest_img
-#from src.xy_img_points_click import point_return
 from src.keep_above_points import keep_img_above_points
 from src.face_contour_width import img_head_contour
 from src.distance_between_points import dis_in_points
@@ -30,38 +29,45 @@ def point_return(img):
     cv2.namedWindow('img')
     cv2.setMouseCallback('img', point_select)
 
+    print("before imgshow")
 
-    while True:
-        cv2.imshow("img", img)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    cv2.imshow('img', img)
+    print("imshow")
+    cv2.waitKey(0)
+    print("waitkey")
     cv2.destroyAllWindows()
 
+    print("window destroyed return points")
     return points[-2], points[-1]
 
 
 def side_mm_metrics(path):
     img_array = split_frames(path)
-    pixel_mm, mag_xy = video_to_pixel_mm(img_array)
+    pixel_mm = video_to_pixel_mm(img_array)
 
     side_head_img = widest_img(img_array)
+
     if side_head_img is None:
         print("side_head_img is None")
+
     else:
         print("select front to nape")
         front, nape = point_return(side_head_img)
+        print (str(front))
 
         img_duplicate = side_head_img.copy()
+        print("img duplicated")
 
         final_img = keep_img_above_points(img_duplicate,  front, nape)
-        final_img = [final_img]
-        _, _, front2nape = img_head_contour(final_img)
+
+        print("select front to nape")
+        contour, front2nape = img_head_contour(final_img)
 
         print("select length points")
         eyebrows, back = point_return(side_head_img)
 
         length = dis_in_points( eyebrows,  back)
+        front2nape_mm = int(front2nape * pixel_mm[0])
+        length_mm = int(length * pixel_mm[0])
 
-        front2nape = int(front2nape * pixel_mm[0])
-        length = int(length * pixel_mm[0])
-    return front2nape, length ,
+    return front2nape_mm, length_mm
