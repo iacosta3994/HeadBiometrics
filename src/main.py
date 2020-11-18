7e1c7d98518f461164cbbfc5672056aea5e560ab
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-11-17 23:23:59 -0800

    still testing commiting for workstation restart.

--- src/main.py
+++ src/main.py
@@ -3,12 +3,15 @@ import sys
 import numpy as np
 
 
+# protocol://host:port/script_name?script_params|auth
+
+
 from src.front_quantify import front_mm_metrics
 from src.side_quantify import side_mm_metrics
 
 
 def main(f_path, s_path):
-    ear_to_ear_mm, head_width_mm = front_mm_metrics(f_path)
+    ear_to_ear_mm, head_width_mm  = front_mm_metrics(f_path)
     front2nape_mm, length_mm = side_mm_metrics(s_path)
 
     circumference_mm = (((head_width_mm * 2) + (length_mm * 2)) * 0.834626841674)
@@ -17,5 +20,5 @@ def main(f_path, s_path):
 
 
 ear_to_ear_mm, front2nape_mm, circumference_mm = main(
-    'Video_Tests/A_test_self.mp4', 'Video_Tests/B_test_self.mp4')
+    'Video_Tests/Test_Video_from_Desma/angie-1.MOV', 'Video_Tests/Test_Video_from_Desma/angie-1.MOV')
 print(ear_to_ear_mm, front2nape_mm, circumference_mm)
