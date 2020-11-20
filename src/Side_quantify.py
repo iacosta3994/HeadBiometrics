ed6db5d42a9c1a6f66d63583d413c76366cbf2c1
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-11-19 21:01:48 -0800

    put pointselect in pr to remove global var, print statements track prog

--- src/Side_quantify.py
+++ src/Side_quantify.py
@@ -1,73 +1,75 @@
-import os
-import sys
 import numpy as np
-import matplotlib
 import cv2
 import copy
 from src.key_frame_extraction import split_frames
 from src.video_to_mm import video_to_pixel_mm
 from src.narrow_wide_img_select import widest_img
 from src.keep_above_points import keep_img_above_points
-from src.face_contour_width import img_head_contour
+from src.face_contour_width import img_head_contour_side
 from src.distance_between_points import dis_in_points
 
-points = []
-def point_select(event, x, y, flags, params):
-    global points
 
-    if event == cv2.EVENT_LBUTTONDOWN:
-        print(x, " ", y)
-        points.append((x, y))
-    elif event == cv2.EVENT_LBUTTONUP:
-        print(x, " ", y)
-        points.append((x, y))
 
 def point_return(img):
-    global points
+    points = []
+
+    def point_select(event, x, y, flags, params):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            points.append([x, y])
+            print(x, " ", y)
+        elif event == cv2.EVENT_LBUTTONUP:
+            points.append([x, y])
+            print(x, " ", y)
 
 
     cv2.namedWindow('img')
+    print("after named window")
+
     cv2.setMouseCallback('img', point_select)
+    print("after setMouseCallback")
 
-    print("before imgshow")
-
-    cv2.imshow('img', img)
+    cv2.imshow("img", img)
     print("imshow")
-    cv2.waitKey(0)
-    print("waitkey")
-    cv2.destroyAllWindows()
+    
+    while True:
+        key = cv2.waitKey(0) & 0xFF
+        if key == ord('q'):
+            print("break")
+            cv2.destroyAllWindows()
+            cv2.waitKey(1)
+            return points[-2], points[-1]
 
-    print("window destroyed return points")
-    return points[-2], points[-1]
 
 
 def side_mm_metrics(path):
     img_array = split_frames(path)
-    pixel_mm = video_to_pixel_mm(img_array)
+    print("img_array: complete")
 
     side_head_img = widest_img(img_array)
-
+    print('side_head_img: complete')
     if side_head_img is None:
         print("side_head_img is None")
-
     else:
-        print("select front to nape")
-        front, nape = point_return(side_head_img)
-        print (str(front))
-
-        img_duplicate = side_head_img.copy()
-        print("img duplicated")
-
-        final_img = keep_img_above_points(img_duplicate,  front, nape)
-
-        print("select front to nape")
-        contour, front2nape = img_head_contour(final_img)
 
         print("select length points")
         eyebrows, back = point_return(side_head_img)
-
+        print("eyebrows, back: complete")
         length = dis_in_points( eyebrows,  back)
-        front2nape_mm = int(front2nape * pixel_mm[0])
-        length_mm = int(length * pixel_mm[0])
+        print('length complete')
 
-    return front2nape_mm, length_mm
+
+        print("select front to nape")
+        front, nape = point_return(side_head_img)
+        print('front and nape: complete')
+        img_duplicate = side_head_img.copy()
+        __, front2nape = img_head_contour_side(img_duplicate, front, nape, ret_contour = True)
+        print('front2nape: complete')
+
+
+        pixel_mm = video_to_pixel_mm(img_array)
+        print("pixel_mm: complete")
+        front2nape = int(front2nape * pixel_mm[0])
+        print('final front2nape: complete')
+        length = int(length * pixel_mm[0])
+        print('final length: complete')
+    return front2nape, length
