cb8227052f7aa00e9f716b227874fce45331a7d4
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-11-28 14:30:43 -0800

    added test funtions, added new module that preruns for one video forboth

--- src/main.py
+++ src/main.py
@@ -6,8 +6,10 @@ import numpy as np
 # protocol://host:port/script_name?script_params|auth
 
 
-from src.front_quantify import front_mm_metrics
-from src.side_quantify import side_mm_metrics
+from src.front_quantify import front_mm_metrics, front_mm_metrics_postmtrp
+from src.side_quantify import side_mm_metrics, side_mm_metrics_postmtrp
+from src.key_frame_extraction import split_frames
+from src.video_to_mm import video_to_pixel_mm
 
 
 def main(f_path, s_path):
@@ -16,9 +18,30 @@ def main(f_path, s_path):
 
     circumference_mm = (((head_width_mm * 2) + (length_mm * 2)) * 0.834626841674)
 
-    return ear_to_ear_mm, front2nape_mm, circumference_mm
+    return circumference_mm, front2nape_mm, ear_to_ear_mm, length_mm, head_width_mm
 
 
-ear_to_ear_mm, front2nape_mm, circumference_mm = main(
-    'Video_Tests/Test_Video_from_Desma/angie-1.MOV', 'Video_Tests/Test_Video_from_Desma/angie-1.MOV')
-print(ear_to_ear_mm, front2nape_mm, circumference_mm)
+def run(path, ios):
+    img_array = split_frames(path, ios)
+
+    pixel_mm = video_to_pixel_mm(img_array)
+
+    ear_to_ear_mm, head_width_mm  = front_mm_metrics_postmtrp(img_array, pixel_mm)
+
+    front2nape_mm, length_mm = side_mm_metrics_postmtrp(img_array, pixel_mm)
+
+    circumference_mm = int(((head_width_mm * 2) + (length_mm * 2)) * 0.834626841674)
+
+    return circumference_mm, front2nape_mm, ear_to_ear_mm, length_mm, head_width_mm
+
+
+#circumference_mm, front2nape_mm, ear_to_ear_mm, length_mm, head_width_mm = run('Videoq_Tests/Test_Video_from_Desma/jada-1.MOV')
+#circumference_mm, front2nape_mm, ear_to_ear_mm, length_mm, head_width_mm = run('Video_Tests/Test_Video_from_Desma/avery-1.MOV')
+#circumference_mm, front2nape_mm, ear_to_ear_mm, length_mm, head_width_mm = run('Video_Tests/Test_Video_from_Desma/desma-1.MOV')
+#circumference_mm, front2nape_mm, ear_to_ear_mm, length_mm, head_width_mm = run('Video_Tests/Test_Video_from_Desma/james-1.MOV')
+#circumference_mm, front2nape_mm, ear_to_ear_mm, length_mm, head_width_mm = run('Video_Tests/Test_Video_from_Desma/angie-1.MOV')
+#circumference_mm, front2nape_mm, ear_to_ear_mm, length_mm, head_width_mm = run('Video_Tests/Test_Video_from_Desma/jay-1.MOV', ios=True)
+circumference_mm, front2nape_mm, ear_to_ear_mm, length_mm, head_width_mm = run('Video_Tests/Self.mp4', ios = False)
+
+
+print(circumference_mm, front2nape_mm, ear_to_ear_mm, length_mm, head_width_mm)
