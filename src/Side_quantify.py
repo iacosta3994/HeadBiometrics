30fdb7a03c2fa7932f0eecd50fa6c7018f70b514
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-11-28 14:32:13 -0800

    pycache

--- src/Side_quantify.py
+++ src/Side_quantify.py
@@ -5,8 +5,7 @@ from src.key_frame_extraction import split_frames
 from src.video_to_mm import video_to_pixel_mm
 from src.narrow_wide_img_select import widest_img
 from src.keep_above_points import keep_img_above_points
-from src.face_contour_width import img_head_contour_side
-from src.distance_between_points import dis_in_points
+from src.face_contour_width import img_head_contour_side_SMPL
 
 
 
@@ -16,60 +15,100 @@ def point_return(img):
     def point_select(event, x, y, flags, params):
         if event == cv2.EVENT_LBUTTONDOWN:
             points.append([x, y])
-            print(x, " ", y)
-        elif event == cv2.EVENT_LBUTTONUP:
+        if event == cv2.EVENT_LBUTTONUP:
             points.append([x, y])
-            print(x, " ", y)
 
 
     cv2.namedWindow('img')
-    print("after named window")
 
     cv2.setMouseCallback('img', point_select)
-    print("after setMouseCallback")
+
 
     cv2.imshow("img", img)
-    print("imshow")
-    
+
+
     while True:
         key = cv2.waitKey(0) & 0xFF
+
         if key == ord('q'):
-            print("break")
+
             cv2.destroyAllWindows()
-            cv2.waitKey(1)
+            cv2.waitKey(0)
             return points[-2], points[-1]
 
+def dis_in_points(pointA, pointB):
+    (xA, yA) = pointA
+    (xB, yB) = pointB
+
+    return round(np.sqrt((xA-xB)**2 + (yA - yB)**2))
 
 
 def side_mm_metrics(path):
     img_array = split_frames(path)
-    print("img_array: complete")
+
 
     side_head_img = widest_img(img_array)
-    print('side_head_img: complete')
+
+    if side_head_img is None:
+        print('Error: can not use image')
+    else:
+
+        def solve_length(img):
+            print("select length")
+            eyebrows, back = point_return(img)
+
+            length = dis_in_points( eyebrows,  back)
+
+            return length
+
+        side_head_img_length = side_head_img.copy()
+        length = solve_length(side_head_img_length)
+
+
+        def solve_f2nape(img):
+            print("select front to nape")
+            front, nape = point_return(img)
+            front2nape = img_head_contour_side_SMPL(img, front, nape)
+            return front2nape
+
+        side_head_img_front2nape = side_head_img.copy()
+        front2nape = solve_f2nape(side_head_img_front2nape)
+
+        pixel_mm = video_to_pixel_mm(img_array)
+
+        front2nape = int(front2nape * pixel_mm[0])
+
+        angle_correction = np.sqrt(pixel_mm[0]/2)
+        length = int(length * angle_correction)
+
+    return front2nape, length
+
+
+def side_mm_metrics_postmtrp(img_array, pixel_mm):
+    side_head_img = widest_img(img_array)
     if side_head_img is None:
         print("side_head_img is None")
     else:
 
-        print("select length points")
-        eyebrows, back = point_return(side_head_img)
-        print("eyebrows, back: complete")
-        length = dis_in_points( eyebrows,  back)
-        print('length complete')
+        def solve_length(img):
+            print("select length points")
+            eyebrows, back = point_return(img)
+            length = dis_in_points( eyebrows,  back)
+            return length
+        side_head_img_length = side_head_img.copy()
+        length = solve_length(side_head_img_length)
 
 
-        print("select front to nape")
-        front, nape = point_return(side_head_img)
-        print('front and nape: complete')
-        img_duplicate = side_head_img.copy()
-        __, front2nape = img_head_contour_side(img_duplicate, front, nape, ret_contour = True)
-        print('front2nape: complete')
+        def solve_f2nape(img):
+            print("select front to nape")
+            front, nape = point_return(img)
+            front2nape = img_head_contour_side_SMPL(img, front, nape)
+            return front2nape
 
-
-        pixel_mm = video_to_pixel_mm(img_array)
-        print("pixel_mm: complete")
+        side_head_img_front2nape = side_head_img.copy()
+        front2nape = solve_f2nape(side_head_img_front2nape)
         front2nape = int(front2nape * pixel_mm[0])
-        print('final front2nape: complete')
+        front2nape = int(front2nape * .5)
         length = int(length * pixel_mm[0])
-        print('final length: complete')
+
     return front2nape, length
