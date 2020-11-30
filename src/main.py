792fb761ac5ebcd64809726ca249a34a44ba299d
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-11-30 15:31:39 -0800

    removed unused imports removing redundancy

--- src/main.py
+++ src/main.py
@@ -6,29 +6,22 @@ import numpy as np
 # protocol://host:port/script_name?script_params|auth
 
 
-from src.front_quantify import front_mm_metrics, front_mm_metrics_postmtrp
-from src.side_quantify import side_mm_metrics, side_mm_metrics_postmtrp
+from src.front_quantify import front_mm_metrics_postmtrp
+from src.side_quantify import side_mm_metrics_postmtrp
 from src.key_frame_extraction import split_frames
 from src.video_to_mm import video_to_pixel_mm
-
-
-def main(f_path, s_path):
-    ear_to_ear_mm, head_width_mm  = front_mm_metrics(f_path)
-    front2nape_mm, length_mm = side_mm_metrics(s_path)
-
-    circumference_mm = (((head_width_mm * 2) + (length_mm * 2)) * 0.834626841674)
-
-    return circumference_mm, front2nape_mm, ear_to_ear_mm, length_mm, head_width_mm
-
+from src.narrow_wide_img_select import narrow_wide_img
 
 def run(path, ios):
     img_array = split_frames(path, ios)
 
     pixel_mm = video_to_pixel_mm(img_array)
 
-    ear_to_ear_mm, head_width_mm  = front_mm_metrics_postmtrp(img_array, pixel_mm)
+    narrow_img, wide_img = narrow_wide_img(img_array)
 
-    front2nape_mm, length_mm = side_mm_metrics_postmtrp(img_array, pixel_mm)
+    ear_to_ear_mm, head_width_mm  = front_mm_metrics_postmtrp(narrow_img, pixel_mm)
+
+    front2nape_mm, length_mm = side_mm_metrics_postmtrp(wide_img, pixel_mm)
 
     circumference_mm = int(((head_width_mm * 2) + (length_mm * 2)) * 0.834626841674)
 
