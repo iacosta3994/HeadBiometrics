747e0599234ab4033c9b2831c53a3e786fa41197
iacosta3994 <46463653+iacosta3994@users.noreply.github.com>
2020-11-30 18:20:02 -0800

    clarified name

--- src/Front_quantify.py
+++ src/Front_quantify.py
@@ -2,14 +2,14 @@ import numpy as np
 import cv2
 
 from src.narrow_wide_img_select import narrowest_img
-from src.face_contour_width import img_head_contour_front
+from src.face_contour_width import ned_front
 from src.contour_width_max import ret_contour_width
 
 
 
 def front_mm_metrics_postmtrp(narrow_img, pixel_mm):
 
-        main_contour, main_contour_length = img_head_contour_front(narrow_img)
+        main_contour, main_contour_length = ned_front(narrow_img)
 
         head_width_mm = int( ret_contour_width(main_contour)  * pixel_mm[0])
 
